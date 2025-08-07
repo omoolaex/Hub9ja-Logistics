10d7a0659f32d29166fd53fadbae7b5661e26007
Omoola003 <owolabigbolahan003@gmail.com>
2025-08-07 17:35:08 +0100

    google tag fixed

--- app/layout.js
+++ app/layout.js
@@ -80,7 +80,7 @@ export default function RootLayout({ children }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GT-T9WXSSQ7');
+            })(window,document,'script','dataLayer','GTM-55CLWM44');
           `}
         </Script>
       </head>
@@ -90,7 +90,7 @@ export default function RootLayout({ children }) {
         {/* ✅ NoScript fallback for GTM */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GT-T9WXSSQ7"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-55CLWM44"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
